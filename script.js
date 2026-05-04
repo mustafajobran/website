1b40b1f14208a0d1ba156ea8570916f20b7c8299
mustafa jubran <mustafajobran80@gimal.com>
2026-05-04 15:57:22 +0300

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+function showsection(showsectionId){
+    let section= document.querySelectorAll(".section");
+
+    sections.array.forEach(element => {
+        section.style.display="none";
+        
+    });
+        
+    document.getElementById(sectionId).style.display="plock";
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
